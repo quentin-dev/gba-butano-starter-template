224a4cc9a17fa05fcd9b6c1326281c68decac7b5
Quentin Barbarat <quentin.barbarat@epita.fr>
2021-04-22 23:07:59 +0200

    add bg

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,22 @@
 #include "bn_keypad.h"
 #include "bn_sprite_text_generator.h"
 #include "bn_sprite_animate_actions.h"
+#include "bn_regular_bg_ptr.h"
 #include "bn_vector.h"
 
 #include "gj_big_sprite_font.h"
 #include "bn_sprite_items_ninja.h"
 #include "bn_sprite_items_pipe.h"
+#include "bn_regular_bg_items_bg_day.h"
 
 int main()
 {
     bn::core::init();
 
     bn::bg_palettes::set_transparent_color(bn::color(0, 0, 0));
+
+    bn::regular_bg_ptr day_bg = bn::regular_bg_items::bg_day.create_bg(0, 0);
+    day_bg.set_visible(true);
 
     bn::sprite_text_generator text_generator(gj::big_sprite_font);
 
@@ -41,6 +46,8 @@ int main()
         {
             ninja_sprite.set_x(ninja_sprite.x() - 1);
         }
+
+        day_bg.set_x(day_bg.x() - 1);
 
         bn::core::update();
         action.update();
